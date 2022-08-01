3df96447457abde9b8dd48e50611077cd340de40
Alex Moreano <alxstaleycobain@hotmail.com>
2022-07-31 22:33:08 -0500

    Created project section

---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './projects.css';
+import Upwork from '../../img/Upwork.png';
+import Fiverr from '../../img/fiverr.png';
+import Amazon from '../../img/amazon.png';
+import Shopify from '../../img/Shopify.png';
+import Facebook from '../../img/Facebook.png';
+
+const projects = () => {
+  return (
+    <div className="projects">
+         <div className="s-leftside">
+            <span>He trabajado con</span>
+            <span>proyectos en:</span>
+            <spane>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima sunt illum ab 
+                <br />
+                veniam quis quibusdam!
+                <br />
+                veniam quis quibusdam!
+                <br />
+                veniam quis quibusdam!
+            </spane>
+            <button className="button i-button">Contratame!</button>
+            <div className="r-blur s-blur" style={{background:'#ABF1FF94'}}></div>
+        </div>
+        {/*right side */}
+        <div className="p-right">
+            <div className="p-mainCircle">
+                <div className="p-secCircle">
+                    <img src={Upwork} alt="" />
+                </div>
+                <div className="p-secCircle">
+                    <img src={Fiverr} alt="" />
+                </div>
+                <div className="p-secCircle">
+                    <img src={Amazon} alt="" />
+                </div>
+                <div className="p-secCircle">
+                    <img src={Shopify} alt="" />
+                </div>
+                <div className="p-secCircle">
+                    <img src={Facebook} alt="" />
+                </div>
+            </div>
+            {/*BACK circles */}
+            <div className="p-backCircle blueCircle"></div>
+            <div className="p-backCircle yellowCircle"></div>
+        </div>
+    </div>
+  )
+};
+
+export default projects;
--- src/portfolioAlexMoreano.jsx
+++ src/portfolioAlexMoreano.jsx
@@ -2,15 +2,17 @@ import Navbar from "./components/navbar/navbar";
 import Intro from "./components/intro/intro";
 import Services from "./components/services/services";
 import Experience from "./components/experience/experience";
+import Projects from "./components/projects/projects";
 import './portfolio.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Intro/>
-      <Services/>
-      <Experience/>
+      <Navbar />
+      <Intro />
+      <Services />
+      <Experience />
+      <Projects />
     </div>
   );
 }
